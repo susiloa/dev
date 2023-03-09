22b2cd90fe82b4884446b8bd5162dcae7a853533
Angela Susilo <you@example.com>
2023-03-09 14:25:19 +0000

    Updated app message

--- labs/continuous-integration/src/HelloWorld/Program.cs
+++ labs/continuous-integration/src/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello EIC Cohort 8!");
         if (args.Length == 1)
         {
             int bedtimeHour = 22;
